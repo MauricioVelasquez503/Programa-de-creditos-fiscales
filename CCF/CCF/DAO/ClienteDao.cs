7daa07f9635d9b585906f07e7c888736710a37f5
Mauricio Velasquez <mauricio.velasquez503@gmail.com>
2022-06-18 19:39:58 -0600

    Proyecto utilizando DAO

---
+++ CCF/CCF/DAO/ClienteDao.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using CCF.DTO;
+
+namespace CCF.DAO
+{
+    internal class ClienteDao : ConexionBD
+    {
+
+        SqlDataReader Leerfilas;
+        SqlCommand Comando = new SqlCommand();
+
+        //METODOS CRUD
+        public List<ClientesDto> VerRegistros(string condicion)
+        {
+            Comando.Connection = Conexion;
+            Comando.CommandText = "VerClientes";
+            Comando.CommandType = CommandType.StoredProcedure;
+            Comando.Parameters.AddWithValue("@Condicion",condicion);
+
+            Conexion.Open();
+            Leerfilas = Comando.ExecuteReader();
+
+            List<ClientesDto> ListaGenerica = new List<ClientesDto>();
+            while (Leerfilas.Read()) 
+            {
+                ListaGenerica.Add(new ClientesDto
+                {
+                    Nrc = Leerfilas.GetString(0),
+                    Razon = Leerfilas.GetString(1),
+                    Direccion = Leerfilas.GetString(2),
+                    Nit = Leerfilas.GetString(3),
+                    Giro = Leerfilas.GetString(4),
+                });
+            }
+            Leerfilas.Close();
+            Conexion.Close();
+
+            return ListaGenerica;
+        }
+
+        public void Insert() { }
+        public void Edit(){}
+        public void Delete() { }
+
+    }
+}
---
+++ CCF/CCF/DTO/ClientesDto.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CCF.DTO
+{
+    internal class ClientesDto
+    {
+        //ATRIBUTOS
+
+        string _Nrc;
+        string _Razon;
+        string _Direccion;
+        string _Nit;
+        string _Giro;
+
+        public string Nrc { get => _Nrc; set => _Nrc = value; }
+
+        public string Razon { get => _Razon; set => _Razon = value; }
+
+        public string Direccion { get => _Direccion; set => _Direccion = value; }
+        
+        public string Nit { get => _Nit; set => _Nit = value; }
+        public string Giro { get => _Giro; set => _Giro = value; }
+     
+
+
+
+        //PROPIEDADES GETTER AND SETTERS
+
+    }
+}
